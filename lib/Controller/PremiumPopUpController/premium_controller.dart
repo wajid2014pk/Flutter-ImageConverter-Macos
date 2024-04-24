faa5a2f0f6d917dfbe4962bc04f84f51f0b6e682
EclixTech <allan.walks.196@gmail.com>
2024-04-24 12:32:01 +0500

    24-04-24: Trigger premium after purchase cancel

--- lib/Controller/PremiumPopUpController/premium_controller.dart
+++ lib/Controller/PremiumPopUpController/premium_controller.dart
@@ -202,141 +202,6 @@ class PayWallController extends GetxController {
     );
   }
 
-  // premiumContainer(String title, String subscriptionDuration, int index,
-  //     bool showTrailText, BuildContext context) {
-  //   return SizedBox(
-  //     width: 160,
-  //     child: Stack(
-  //       children: [
-  //         Obx(
-  //           () => Column(
-  //             crossAxisAlignment: CrossAxisAlignment.center,
-  //             children: [
-  //               InkWell(
-  //                 hoverColor: Colors.transparent,
-  //                 onTap: () => {selectPackage.value = index},
-  //                 child: Container(
-  //                   // width: 190,
-  //                   height: 165,
-  //                   decoration: BoxDecoration(
-  //                     border: Border.all(
-  //                       color: selectPackage.value == index
-  //                           ? UiColors.lightblueColor
-  //                           : Colors.white,
-  //                       style: BorderStyle.solid,
-  //                       width: 2.2,
-  //                     ),
-  //                     color: Colors.white,
-  //                     borderRadius: BorderRadius.circular(
-  //                       10,
-  //                     ),
-  //                   ),
-  //                   child: Padding(
-  //                     padding: const EdgeInsets.only(top: 0, bottom: 0),
-  //                     child: Column(
-  //                       mainAxisAlignment: MainAxisAlignment.start,
-  //                       children: [
-  //                         Container(
-  //                           width: double.infinity,
-  //                           height: 40,
-  //                           decoration: BoxDecoration(
-  //                               color: selectPackage.value == index
-  //                                   ? UiColors.lightblueColor
-  //                                   : Colors.white,
-  //                               borderRadius: const BorderRadius.only(
-  //                                 topRight: Radius.circular(8),
-  //                                 topLeft: Radius.circular(8),
-  //                               )),
-  //                           child: SizedBox(
-  //                             width: 10,
-  //                             child: Center(
-  //                               child: Text(
-  //                                 title,
-  //                                 maxLines: 1,
-  //                                 overflow: TextOverflow.ellipsis,
-  //                                 style: GoogleFonts.poppins(
-  //                                   color: selectPackage.value == index
-  //                                       ? UiColors.whiteColor
-  //                                       : UiColors.lightblueColor,
-  //                                   fontSize: 16,
-  //                                   fontWeight: FontWeight.w500,
-  //                                 ),
-  //                               ),
-  //                             ),
-  //                           ),
-  //                         ),
-  //                         Expanded(
-  //                           child: Container(
-  //                             width: MediaQuery.sizeOf(context).width,
-  //                             decoration: BoxDecoration(
-  //                               color: Colors.grey.withOpacity(0.1),
-  //                               borderRadius: const BorderRadius.only(
-  //                                 bottomLeft: Radius.circular(12),
-  //                                 bottomRight: Radius.circular(12),
-  //                               ),
-  //                             ),
-  //                             child: Column(
-  //                               mainAxisAlignment: MainAxisAlignment.start,
-  //                               crossAxisAlignment: CrossAxisAlignment.center,
-  //                               children: [
-  //                                 Padding(
-  //                                   padding: const EdgeInsets.only(top: 8.0),
-  //                                   child: SizedBox(
-  //                                     width: 170,
-  //                                     child: Text(
-  //                                       subscriptionDuration,
-  //                                       textAlign: TextAlign.center,
-  //                                       maxLines: 1,
-  //                                       overflow: TextOverflow.ellipsis,
-  //                                       style: GoogleFonts.poppins(
-  //                                         fontWeight: FontWeight.w500,
-  //                                         fontSize: 20,
-  //                                       ),
-  //                                     ),
-  //                                   ),
-  //                                 ),
-  //                                 Text(
-  //                                   index == 0
-  //                                       ? "${offerings.current!.availablePackages[4].storeProduct.currencyCode} ${getActualPrice(offerings.current!.availablePackages[3].storeProduct.price, 30)}"
-  //                                       : "${offerings.current!.availablePackages[3].storeProduct.currencyCode} ${getActualPrice(offerings.current!.availablePackages[4].storeProduct.price, 60)}",
-  //                                   style: GoogleFonts.poppins(
-  //                                       fontWeight: FontWeight.w400,
-  //                                       fontSize: 18,
-  //                                       color: Colors.black.withOpacity(0.5),
-  //                                       decoration: TextDecoration.lineThrough),
-  //                                 ),
-  //                                 const SizedBox(
-  //                                   height: 12.0,
-  //                                 ),
-  //                                 Text(
-  //                                   index == 0
-  //                                       ? "${offerings.current!.availablePackages[4].storeProduct.currencyCode} ${offerings.current!.availablePackages[4].storeProduct.price.toStringAsFixed(2)}"
-  //                                       : "${offerings.current!.availablePackages[3].storeProduct.currencyCode} ${offerings.current!.availablePackages[3].storeProduct.price.toStringAsFixed(2)}",
-  //                                   style: GoogleFonts.poppins(
-  //                                     fontWeight: FontWeight.w600,
-  //                                     fontSize: 22,
-  //                                     color: selectPackage.value == index
-  //                                         ? UiColors.lightblueColor
-  //                                         : Colors.black,
-  //                                   ),
-  //                                 ),
-  //                               ],
-  //                             ),
-  //                           ),
-  //                         )
-  //                       ],
-  //                     ),
-  //                   ),
-  //                 ),
-  //               ),
-  //             ],
-  //           ),
-  //         ),
-  //       ],
-  //     ),
-  //   );
-  // }
-
   getActualPrice(double salePrice, double disRate) {
     disRate = disRate / 100;
     double disRatePer = 1 - disRate;
@@ -414,7 +279,8 @@ class PayWallController extends GetxController {
         Get.back();
       }
     } on PlatformException catch (e) {
-      Get.back();
+      // Get.back();
+      Get.offAll(() => const HomeScreen());
       ScaffoldMessenger.of(Get.context!).showSnackBar(SnackBar(
           duration: const Duration(seconds: 2),
           behavior: SnackBarBehavior.floating,
